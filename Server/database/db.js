af7a5768d69876679af28bdbc533c9e1610ce369
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-08-20 14:56:04 +0545

    database created

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,8 +1,8 @@
-// const mongoose = require('mongoose'); //third party
+const mongoose = require('mongoose'); //third party
 
-// mongoose.connect('mongodb://127.0.0.1:27017/PlayME',{
-// //to remove deprecated warnings....
-// useNewUrlParser:true,
-// useCreateIndex:true,
-// useUnifiedTopology : true
-// })
+mongoose.connect('mongodb://127.0.0.1:27017/PlayME',{
+//to remove deprecated warnings....
+useNewUrlParser:true,
+useCreateIndex:true,
+useUnifiedTopology : true
+})
