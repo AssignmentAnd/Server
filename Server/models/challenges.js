e75738453da089836af2ab6fb488a2980cc52b03
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-08-20 15:13:02 +0545

    challenge table

--- Server/models/challenges.js
+++ Server/models/challenges.js
@@ -1,18 +1,18 @@
-// const mongoose = require('mongoose'); //third party
+const mongoose = require('mongoose'); //third party
 
-// const Challenges = mongoose.model('Hire',{
-//     Challenger: {
-//         type:String,
-//         required : true
-//     },
-//     Phone: {
-//         type: String,
-//         required: true
-//     },
-//     Challenge:{
-//         type : String,
-//         required: true
-//     },
+const Challenges = mongoose.model('Hire',{
+    Challenger: {
+        type:String,
+        required : true
+    },
+    Phone: {
+        type: String,
+        required: true
+    },
+    Challenge:{
+        type : String,
+        required: true
+    },
 
-// })
-// module.exports = Challenges;
+})
+module.exports = Challenges;
