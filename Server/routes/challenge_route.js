7689472331c4d30e0d9255df04b2095e33cc2e7d
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-08-24 15:33:12 +0545

    challenge added

--- Server/routes/challenge_route.js
+++ Server/routes/challenge_route.js
@@ -1,41 +1,41 @@
-// const express = require('express');
-// const { check, validationResult } = require('express-validator');
-// const bcryptjs = require('bcryptjs');
-// const Challenges = require('../models/challenges');
-// const jwt = require('jsonwebtoken');
-// const router = express.Router();
-// const auth = require('../Middleware/Authenticate')
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const Challenges = require('../models/challenges');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const auth = require('../Middleware/Authenticate')
 
-// router.post('/challenge/insert', function (req, res) {
-//     console.log(req.body)
+router.post('/challenge/insert', function (req, res) {
+    console.log(req.body)
    
     
-//         //valid
-//         const Challenger = req.body.Challenger;
-//         const Phone = req.body.Phone;
-//         const Challenge = req.body.Challenge;
-//            // console.log(us);
-//         // console.log(add); 
+        //valid
+        const Challenger = req.body.Challenger;
+        const Phone = req.body.Phone;
+        const Challenge = req.body.Challenge;
+           // console.log(us);
+        // console.log(add); 
         
-//             const data = new Challenges({
-//                 Challenger: Challenger,
-//                 Phone: Phone,
-//                 Challenge: Challenge
+            const data = new Challenges({
+                Challenger: Challenger,
+                Phone: Phone,
+                Challenge: Challenge
              
-//             });
+            });
 
-//             data.save()
+            data.save()
             
-//             .then(function (result) {
+            .then(function (result) {
                 
-//                 console.log(data)
-//                 return res.status(201).json({ success:true,message: "challenge success !!!!" })
-//             })// sucessess vayo ki vaena
-//             .catch(function (err45) {
-//                 console.log(err45)
-//                 res.status(500).json({ error: err45 })
-//             })// error aayo ki aayena
-//         })
+                console.log(data)
+                return res.status(201).json({ success:true,message: "challenge success !!!!" })
+            })// sucessess vayo ki vaena
+            .catch(function (err45) {
+                console.log(err45)
+                res.status(500).json({ error: err45 })
+            })// error aayo ki aayena
+        })
         
 // router.get('/challenge/show', function (req, res) {
 //     // console.log("this is for showing data")
